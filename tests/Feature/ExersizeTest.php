748f7102fb7ca63d554c6c8d8c87de8636804a84
oksanaozcan <mekansa83@gmail.com>
2022-06-29 13:51:47 +0300

    routes tests

---
+++ tests/Feature/ExersizeTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExersizeTest extends TestCase
+{
+  public static $value = 0;
+
+  /**
+   * @before
+   */
+  public static function incrementThisValue()
+  {    
+    return self::$value++;    
+  }  
+
+  /**
+   * @after
+   */
+  public function test_debug_output_to_cli() 
+  {
+    var_dump('annotations are working');
+    ob_flush();
+  }
+
+  /**
+   * @dataProvider emails_prodiver
+   */
+  public function test_valid_email($email)
+  {
+    $this->assertMatchesRegularExpression('/^.+\@\S+\.\S+$/', $email);
+  }
+
+  public function test_assert_equals()
+  {   
+    $this->assertEquals(5, self::$value);
+  }
+
+  /**
+   * @dataProvider numbers_provider
+   */
+  public function test_math($a, $b, $exepted)
+  {
+    $res = $a * $b;
+    $this->assertEquals($exepted, $res);
+  }
+
+   
+
+  
+
+  public function emails_prodiver()
+  {
+    return [
+      ['dsds@ghfd.df'],
+      ['test@test.com'],
+      ['some@gmail.info'],
+    ];
+  }
+
+  public function numbers_provider()
+  {
+    return [
+      [1,2,2],
+      [2,2,4],
+      [3,3,9],
+    ];
+  }
+
+}
--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -6,11 +6,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 use Illuminate\Support\Str;
 
 class RoutesTest extends TestCase
-{
+{   
   public function test_main_route_assert()
   {
     $response = $this->get('/');
@@ -19,10 +20,25 @@ class RoutesTest extends TestCase
     $response->assertStatus(200);
   }
 
+  public function test_contact_route_assert()
+  {
+    $response = $this->get('/contacts');
+    $response->assertSee('Или оставьте заявку:');
+    $response->assertViewIs('contact.index');
+    $response->assertStatus(200);    
+  }
+
+  public function test_gallery_route_assert()
+  {
+    $response = $this->get('/gallery');    
+    $response->assertViewIs('gallery.index');
+    $response->assertStatus(200);
+  }
+
   public function test_assert_redirect_from_register_to_main_route()
   {
     $response = $this->get('/register');
-    $response->assertRedirect('/');
+    $response->assertRedirect('/');    
   }
 
   public function test_assert_redirect_from_admin_to_login()
@@ -36,5 +52,25 @@ class RoutesTest extends TestCase
     $response = $this->get('/login');
     $response->assertStatus(200);
   }  
+
+  public function test_admin_route_auth_work()
+  {
+    $user = User::find(1);
+    $response = $this->actingAs($user)->get('/admin');
+    $response->assertOk();  
+  }
+
+  public function test_auth_middleware_is_working()
+  { 
+    $response = $this->get('/admin/theme');
+    $response->assertRedirect('/login');
+  }
+
+  public function test_status_404_exception()
+  {
+    $this->get('/h')->assertStatus(404);
+    $this->get('/theme')->assertStatus(404);
+    $this->get('/order')->assertStatus(404);   
+  }
   
 }
